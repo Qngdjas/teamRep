114e9b3fd66f74a4b9fc8673fc684fdd1fadb0cc
Qngdjas <qngdjas@mail.ru>
2021-01-26 21:02:51 +0800

    day tasks

--- teamDev1/src/teamdev1/Course.java
+++ teamDev1/src/teamdev1/Course.java
@@ -5,21 +5,40 @@
  */
 package teamdev1;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
 /**
  *
  * @author Qngdjas
  */
+@XmlRootElement(name = "course")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"student", "name"})
 public class Course {
 
-    private Student[] student;
-
+    @XmlElement(name = "student", required = true)
+    private Student student;
+    @XmlAttribute(name = "name", required = true)
     private String name;
 
-    public Student[] getStudent() {
+    public Course() {
+    }
+
+    public Course(String name, Student student) {
+        this.name = name;
+        this.student = student;
+    }
+
+    public Student getStudent() {
         return student;
     }
 
-    public void setStudent(Student[] student) {
+    public void setStudent(Student student) {
         this.student = student;
     }
 
@@ -33,6 +52,6 @@ public class Course {
 
     @Override
     public String toString() {
-        return "ClassPojo [student = " + student + ", name = " + name + "]";
+        return "[student = " + student + ", name = " + name + "]";
     }
 }

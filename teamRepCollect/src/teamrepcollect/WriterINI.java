4a5583aa812efc3fc099c3ed2f966b95c9e39dd8
Qngdjas <qngdjas@mail.ru>
2021-02-03 20:27:12 +0800

    Version 1.0

--- teamRepCollect/src/teamrepcollect/WriterINI.java
+++ teamRepCollect/src/teamrepcollect/WriterINI.java
@@ -13,14 +13,13 @@ public class WriterINI {
 
     /*Метод записи в ini-файл(Вход - содержимое, которое нужно записать в виде
     ArrayList<String>, выход - логическая переменная(запись прошла (не)успешно))*/
-    public static boolean writer(ArrayList<String> text) {
+    public static boolean writer(ArrayList<String> text, File file) {
         //Файл, в который будет записана информация
-        File fileINI = new File("src/teamrepcollect/Input.ini");
         FileWriter fw = null;
         BufferedWriter bw = null;
         PrintWriter pw = null;
         try {
-            fw = new FileWriter(fileINI);
+            fw = new FileWriter(file);
             bw = new BufferedWriter(fw);
             pw = new PrintWriter(bw);
             //Запись в файл
